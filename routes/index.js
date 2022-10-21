52d9042618f2119f8f96fc0b9b2b11bf122b7a00
Mukesh Rajbanshi <mukesh9811369428@gmail.com>
2022-10-21 11:57:47 +0545

    test ejs

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,13 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get("/", function (req, res, next) {
+  res.render("index", { title: "Express", generated_by: "Mukesh" });
+});
+
+router.get("/test", function (req, res, next) {
+  res.render("test", { bob: "one piece film red" });
 });
 
 module.exports = router;
